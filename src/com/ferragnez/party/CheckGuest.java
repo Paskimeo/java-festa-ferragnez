c01a58bed2097bcbe90ce84b00363af1a1d4aa7a
paskimeo <paskimeo@gmail.com>
2022-06-10 15:01:41 +0200

    aggiunta richiesta nome

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,7 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
 	public static void main(String[] args) {
@@ -8,7 +10,15 @@ public class CheckGuest {
 				"J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", 
 				"Martina Maccherone", "Rachel Zeilic"};
 		
-		System.out.println(listaInvitati[3]);
+		Scanner scan = new Scanner(System.in);
+		
+		//sezione richiesta nome
+		 String inserireNome;
+		   
+		      System.out.print("come ti chiami? vediamo se sei nella lista: ");
+		      inserireNome = scan.next();
+		    
+		      System.out.println(inserireNome);
 		}
 
 	}
