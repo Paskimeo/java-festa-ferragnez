225b53a11f51fb2e558f2305d046e235621a7130
paskimeo <paskimeo@gmail.com>
2022-06-10 14:33:29 +0200

    inizio programma e inserito arrey

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,16 @@
+package com.ferragnez.party;
+
+public class CheckGuest {
+
+	public static void main(String[] args) {
+		//creazione stringa lista nomi
+		String[] listaInvitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli",
+				"J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", 
+				"Martina Maccherone", "Rachel Zeilic"};
+		
+		System.out.println(listaInvitati[3]);
+		}
+
+	}
+
+
